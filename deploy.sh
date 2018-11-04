80ad7bd06b31e9d5b8519de033acf6d6783f690f
Release Manager <Release.Manager@jenkins.com.au>
2018-11-04 12:32:06 +1100

    bump version

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-set -e
+RANCHER_ACCESS_KEY=AA57D92145E412E26C0D
+RANCHER_SECRET_KEY=N519UCmNmqq2AD7ti8EYxEVUv1PwyFpcJyGWeqUm
+RANCHER_URL=http://localhost:8080/v2-beta/projects/1a5
+ENV_NAME=lcl
+PROJECT_NAME=auth-service
+BASE_DIR=${PWD}
+TAG_NAME=0.0.1
 
-cd "$(dirname "$0")"
+echo -e "TAG_NAME=$TAG_NAME" > env.txt
 
-while getopts ":p:e:" opt; do
-  case $opt in
-    # Provide commands to run
-    p)
-      PASSWORD="${OPTARG}"
-    ;;
-    e)
-      ENV="${OPTARG}"
-    ;;
-    \?)
-      echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
-done
+echo "Force pulling..."
+rancher-compose --url ${RANCHER_URL} --access-key AA57D92145E412E26C0D --secret-key N519UCmNmqq2AD7ti8EYxEVUv1PwyFpcJyGWeqUm -e env.txt -p expenseManager-${ENV_NAME} -f ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml pull
 
-TAG_NAME=$(<../VERSION)
-echo -e "TAG_NAME=$TAG_NAME" > .env
+echo "Starting deployment..."
+rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -r ${BASE_DIR}/rancher-compose.${PROJECT_NAME}.yml -e env.txt -p expenseManager-${ENV_NAME} -f ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --upgrade -d --pull --batch-size 1
 
-echo "docker login"
-docker login --username=cjmason8 --password=$PASSWORD
-echo "docker pull"
-docker pull cjmason8/auth-service:$TAG_NAME
-echo "docker compose"
-pwd
-docker-compose -f ${ENV}/docker-compose-${ENV}.yml up -d authService
-echo "finished"
+if [ $? -eq 0 ]; then
+  echo "Deploy success! Confirming..."
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager-${ENV_NAME} -f ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --confirm-upgrade -d --batch-size 1
+else
+  echo "Deploy failed :( rolling back..."
+  rancher-compose --url ${RANCHER_URL} --access-key ${RANCHER_ACCESS_KEY} --secret-key ${RANCHER_SECRET_KEY} -e env.txt -p expenseManager-${ENV_NAME} -f ${BASE_DIR}/${ENV_NAME}/docker-compose-${ENV_NAME}.yml up --rollback -d --batch-size 1
+fi
--- runLocal.sh
+++ runLocal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -Djava.security.egd=file:/dev/./urandom -jar /app/authService.jar
+java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8001,suspend=n -Djava.security.egd=file:/dev/./urandom -jar /app/authService.jar
