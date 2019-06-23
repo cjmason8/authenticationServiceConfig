6f22673c81ce2463b4cb9335aaec7a764a66fa75
Chris Mason <cjmason8@gmail.com>
2019-06-23 15:10:05 +1000

    fix up build

--- buildLocal.sh
+++ buildLocal.sh
@@ -23,7 +23,7 @@ echo "Creating image: ${FULL_IMAGE_NAME}:${TAG_NAME}"
 
 #echo "maven"
 cd ../authenticationService
-docker run --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -w /usr/src/mymaven maven:3.6.0-jdk-12-alpine mvn clean install
+docker run --rm -v "$PWD":/usr/src/mymaven -u 1000:1000 -v "$HOME/.m2":/var/maven/.m2 -e MAVEN_CONFIG=/var/maven/.m2 -w /usr/src/mymaven maven:3.6.1-jdk-12 mvn -Duser.home=/var/maven clean install --no-transfer-progress
 cd ../authenticationServiceConfig
 mkdir -p target
 cp ../authenticationService/target/authservice-0.0.1-SNAPSHOT.jar target
